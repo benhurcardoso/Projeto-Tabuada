eae2dba909d94e8e6d93ddbb0125f3c205d543f3
Ben-hur Cardoso Mazarem <131083420+benhurcardoso@users.noreply.github.com>
2024-05-10 22:42:13 -0300

    Iniciando JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-console.log("teste");
+// Seleção de elementos
+const multiplicationform = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+// Funções
+
+// Eventos
+multiplicationform.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+     if(!multiplicationNumber || !multiplicatorNumber) return;
+
+     console.log(multiplicationNumber, multiplicatorNumber);
+})
